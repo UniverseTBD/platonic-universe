cb5d9061a32e5ffa467919f4da42dbdfd91dd909
Kshitij Duraphe <kshitijd@bu.edu>
2026-03-15 23:07:07 -0400

    feat: register specformer model adapter
    Import the specformer module in models __init__ so it self-registers
    with the adapter registry on package load.

--- src/pu/models/__init__.py
+++ src/pu/models/__init__.py
@@ -6,4 +6,5 @@ from .registry import get_adapter, register_adapter, list_adapters
 from . import hf
 from . import astropt
 from . import sam2
+from . import specformer
 __all__ = ["get_adapter", "register_adapter", "list_adapters"]
